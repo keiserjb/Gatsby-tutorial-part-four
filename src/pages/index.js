0fa0399b3258d0fcef5a1e7a570d89e10ddcef89
Justin Keiser <JustinK@modelaircraft.org>
2020-10-22 10:27:58 -0400

    add seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
+
 export default function Home({ data }) {
   console.log(data)
   return (
